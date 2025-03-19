be2eba8b2a55df9d87fb96e2ace61fb548c336f4
padmajapinnika <padmajapinnika@gmail.com>
2025-03-19 11:47:47 -0400

    updated to login api

--- auth.js
+++ auth.js
@@ -20,8 +20,10 @@ module.exports = (router) => {
   router.post('/login', (req, res) => {
     passport.authenticate('local', { session: false }, (error, user, info) => {
       if (error || !user) {
+        console.log("error is:",error);
+        console.log("User exist?",user);
         return res.status(400).json({
-          message: 'Something is not right',
+          message: 'Something is not correct',
           user: user
         });
       }
--- index.js
+++ index.js
@@ -115,52 +115,7 @@ app.get("/director/:name", passport.authenticate('jwt', { session: false }), (re
         });
 });
 
-app.post('/users',
-    // Validation logic here for request
-    //you can either use a chain of methods like .not().isEmpty()
-    //which means "opposite of isEmpty" in plain english "is not empty"
-    //or use .isLength({min: 5}) which means
-    //minimum value of 5 characters are only allowed
-    [
-      check('Username', 'Username is required').isLength({min: 5}),
-      check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
-      check('password', 'password is required').not().isEmpty(),
-      check('email', 'Email does not appear to be valid').isEmail()
-    ], async (req, res) => {
-  
-    // check the validation object for errors
-      let errors = validationResult(req);
-  
-      if (!errors.isEmpty()) {
-        return res.status(422).json({ errors: errors.array() });
-      }
-  
-      let hashedPassword = Users.hashPassword(req.body.password);
-      await Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
-        .then((user) => {
-          if (user) {
-            //If the user is found, send a response that it already exists
-            return res.status(400).send(req.body.Username + ' already exists');
-          } else {
-            Users
-              .create({
-                Username: req.body.Username,
-                password: hashedPassword,
-                email: req.body.email,
-                Birthday: req.body.Birthday
-              })
-              .then((user) => { res.status(201).json(user) })
-              .catch((error) => {
-                console.error(error);
-                res.status(500).send('Error: ' + error);
-              });
-          }
-        })
-        .catch((error) => {
-          console.error(error);
-          res.status(500).send('Error: ' + error);
-        });
-    });
+
 app.put("/users/:Username", passport.authenticate('jwt', { session: false }), async (req, res) => {
     if(req.user.Username !== req.params.Username){
         return res.status(400).send('Permission denied');
